48508e93407fc2e342ae3f1e4834e5ab292e7c0a
Grant Fortington <gwfortington@hotmail.com>
2024-10-13 23:03:20 +1030

    development

--- test/main.ts
+++ test/main.ts
@@ -1,9 +1,71 @@
+import dotenv from 'dotenv';
 import assert from 'node:assert/strict';
 import { after, before, describe, it } from 'node:test';
 
-describe('main', () => {
+import { Debug, MessageType } from 'node-debug';
+import { generateConfig, redactedConfig } from '../dist';
+
+describe('main', (suiteContext) => {
+  Debug.initialize(true);
+  let debug: Debug;
+  const filePath = `${process.cwd()}/.test.env`;
+  const ruleOverrides = {
+    database: () => 'test',
+  };
   before(async () => {});
-  it('<name>', async () => {
+  it('options-none', async (testContext) => {
+    debug = new Debug(`${suiteContext.name}.test.${testContext.name}`);
+    debug.write(MessageType.Entry);
+    dotenv.config({ path: filePath });
+    debug.write(MessageType.Step, `Generating config...`);
+    const config = generateConfig();
+    debug.write(
+      MessageType.Value,
+      `config=${JSON.stringify(redactedConfig(config))}`,
+    );
+    debug.write(MessageType.Exit);
+    assert.ok(true);
+  });
+  it('options-filePath', async (testContext) => {
+    debug = new Debug(`${suiteContext.name}.test.${testContext.name}`);
+    debug.write(MessageType.Entry);
+    debug.write(MessageType.Step, `Generating config...`);
+    const config = generateConfig({ filePath: filePath });
+    debug.write(
+      MessageType.Value,
+      `config=${JSON.stringify(redactedConfig(config))}`,
+    );
+    debug.write(MessageType.Exit);
+    assert.ok(true);
+  });
+  it('options-ruleOverrides', async (testContext) => {
+    debug = new Debug(`${suiteContext.name}.test.${testContext.name}`);
+    debug.write(MessageType.Entry);
+    dotenv.config({ path: filePath });
+    debug.write(MessageType.Step, `Generating config...`);
+    const config = generateConfig({
+      ruleOverrides: ruleOverrides,
+    });
+    debug.write(
+      MessageType.Value,
+      `config=${JSON.stringify(redactedConfig(config))}`,
+    );
+    debug.write(MessageType.Exit);
+    assert.ok(true);
+  });
+  it('options-both', async (testContext) => {
+    debug = new Debug(`${suiteContext.name}.test.${testContext.name}`);
+    debug.write(MessageType.Entry);
+    debug.write(MessageType.Step, `Generating config...`);
+    const config = generateConfig({
+      filePath: filePath,
+      ruleOverrides: ruleOverrides,
+    });
+    debug.write(
+      MessageType.Value,
+      `config=${JSON.stringify(redactedConfig(config))}`,
+    );
+    debug.write(MessageType.Exit);
     assert.ok(true);
   });
   after(async () => {});
